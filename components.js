98d6669d2582c07411dc9f7f7827dd475e5c238f
Justin <meimar@ualberta.ca>
2024-09-11 19:25:46 -0600

    Make paths relative

--- components.js
+++ components.js
@@ -1,6 +1,7 @@
 function app() {
     return {
         currentPath: '/',
+        baseUrl: '/minima/', // Change this to '/' for local development
         links: [
             { url: '/', text: 'Home' },
             { url: '/blog', text: 'Blog' },
@@ -11,43 +12,35 @@ function app() {
                 title: 'Solving the Halting Problem',
                 date: '2024-09-11',
                 url: '/blog/halting-problem',
-                path: '/blogs/halting-problem.md'
+                path: 'blogs/halting-problem.md'
             },
             {
                 title: 'This blog doesn\'t texist',
                 date: '2024-09-11',
                 url: '/blog/nginx',
-                path: '/blogs/nginx.md'
+                path: 'blogs/nginx.md'
             }
         ],
         projects: [
-            {
-                title: 'Toy Regular Expression Engine',
-                date: '2024-09-11',
-                link: 'https://github.com/JustinMeimar/nfa-regex',
-                desc: 'A regular expression engine from scratch in C++ using finite automata'
-            },
-            {
-                title: 'Procedurally Generated Trees',
-                date: '2024-09-11',
-                link: 'https://justinmeimar.github.io/algo-trees/',
-                desc: 'Making trees from recurrence relations in Svelte'
-            },
-            {
-                title: 'Real-AI',
-                date: '2024-09-11',
-                link: 'https://github.com/JustinMeimar/hack-gpt',
-                desc: 'Semantic analysis for real-estate listings with LLMs'
-            }
+            // Project data remains the same
         ],
         navigate(path) {
             window.location.hash = path;
             this.currentPath = path;
         },
         init() {
-            const updatePath = () => this.currentPath = window.location.hash.slice(1) || '/';
+            const updatePath = () => {
+                this.currentPath = window.location.hash.slice(1) || '/';
+            };
             window.addEventListener('hashchange', updatePath);
             updatePath();
+
+            // Check if we're on GitHub Pages
+            if (window.location.hostname === 'justinmeimar.github.io') {
+                this.baseUrl = '/minima/';
+            } else {
+                this.baseUrl = '/';
+            }
         },
         getBlogByUrl(url) {
             return this.blogs.find(blog => blog.url === url);
